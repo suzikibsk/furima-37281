02595703ba1c34cdceb9e866937ff4545367f098
suzikibsk <96762887+suzikibsk@users.noreply.github.com>
2022-04-26 17:34:00 +0900

    新規登録・ログイン画面のビューの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
   
   
   
@@ -9,5 +11,9 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == 'hogehoge' && password == '1111'
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_day])
   end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+
+
+  has_many :items
+  has_many :orders
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+
+  validates :nickname, presence: true, uniqueness: true
+  # @含むこと・存在することはdeviseのデフォルト実装のため省略
+  validates :email,    uniqueness: true
+  # 全角ひらがな、全角カタカナ、漢字
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  # 全角カタカナ
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birth_day, presence: true
+end
