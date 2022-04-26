1e2574b763a9327420cb3c0371028e1b5a736d82
suzikibsk <96762887+suzikibsk@users.noreply.github.com>
2022-04-26 22:33:21 +0900

    新規登録・ログイン画面のビューの実装　変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,17 +9,20 @@ class User < ApplicationRecord
   has_many :items
   has_many :orders
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+  
+  with_options presence: true do
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
 
-  validates :nickname, presence: true, uniqueness: true
-  # @含むこと・存在することはdeviseのデフォルト実装のため省略
-  validates :email,    uniqueness: true
-  # 全角ひらがな、全角カタカナ、漢字
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  # 全角カタカナ
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birth_day, presence: true
+    validates :nickname, presence: true, uniqueness: true
+    # @含むこと・存在することはdeviseのデフォルト実装のため省略
+    validates :email,    uniqueness: true
+    # 全角ひらがな、全角カタカナ、漢字
+    validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    # 全角カタカナ
+    validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birth_day, presence: true
+  end
 end
