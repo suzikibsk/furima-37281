455d407fedab8a73ae2446c0a448afdad65a9d11
suzikibsk <96762887+suzikibsk@users.noreply.github.com>
2022-05-08 17:14:55 +0900

    モデル単体コードテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_many :items
   has_many :orders
 
-  
+
   with_options presence: true do
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
     validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
