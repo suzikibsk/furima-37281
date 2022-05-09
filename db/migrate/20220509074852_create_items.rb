0a0e10a9f22fb104434c9bde0d4eb81e52a30622
suzikibsk <96762887+suzikibsk@users.noreply.github.com>
2022-05-09 17:41:14 +0900

    商品出品情報を保存するためのテーブル作成

--- db/migrate/20220509074852_create_items.rb
+++ db/migrate/20220509074852_create_items.rb
@@ -1,7 +1,16 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.references   :user,              null:false, foreign_key: true
+      t.string       :name,              null:false
+      t.text         :introduction,      null:false
+      t.integer      :category,          null: false
+      t.integer      :item_status_id,    null: false
+      t.integer      :shopping_costs_id, null: false
+      t.integer      :region_id,         null: false
+      t.integer      :shopping_date_id,  null: false
+      t.integer      :price,             null: false
+     
       t.timestamps
     end
   end
