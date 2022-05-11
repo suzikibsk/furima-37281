2dd3278ce9e0d2da872e4c4c58e95bcbc60661b0
suzikibsk <96762887+suzikibsk@users.noreply.github.com>
2022-05-11 14:17:39 +0900

    10000000円以上で出品できないように実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,15 +16,18 @@ class Item < ApplicationRecord
 
   with_options presence: true do
     validates :user_id
+    validates :image
     validates :name
-    validates :description
+    validates :introduction
     validates :category_id
     validates :item_status_id
     validates :shipping_cost_id
-    validates :delivery_area_id
+    validates :region_id
     validates :shipping_date_id
     # 300円以上かつ半角数字でないと入力不可
     validates :price, numericality: { greater_than_or_equal_to: 300 }
+    # 300円以上かつ9,999,999円以下で、半角数字でないと入力不可
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 
   # ジャンルの選択が「--」の時は保存不可
