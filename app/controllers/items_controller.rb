6cca4d6a927c584e02c1230391de1c3f3bede0cc
suzikibsk <96762887+suzikibsk@users.noreply.github.com>
2022-05-13 01:06:15 +0900

    Revert "商品削除機能"2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   #重複処理をまとめる
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   
   
   def index
@@ -41,6 +41,16 @@ class ItemsController < ApplicationController
   end
 
   def show
+  end
+
+  def destroy
+    # ログインしているユーザーと同一であればデータを削除する
+    if @item.user_id == current_user.id
+      @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
   private
