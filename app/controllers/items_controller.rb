ae1b189a3d7d53ec9386209a56e1365fcc3db2eb
suzikibsk <96762887+suzikibsk@users.noreply.github.com>
2022-05-13 01:08:06 +0900

    リファクタリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -47,7 +47,6 @@ class ItemsController < ApplicationController
     # ログインしているユーザーと同一であればデータを削除する
     if @item.user_id == current_user.id
       @item.destroy
-      redirect_to root_path
     else
       redirect_to root_path
     end
