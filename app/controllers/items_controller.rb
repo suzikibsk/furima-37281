1ee543ee0cd01ddfc80f010127ae7c9259b8d004
suzikibsk <96762887+suzikibsk@users.noreply.github.com>
2022-05-12 21:08:56 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-
-  before_action :set_item, only: :show
+  #重複処理をまとめる
+  before_action :set_item, only: [:show, :edit, :update]
   
   
   def index
@@ -21,24 +21,25 @@ class ItemsController < ApplicationController
     end
   end
 
-  #def destroy
-  #  item = Item.find(params[:id])
-  #  item.destroy
-  #end
+  def edit
+    #ログインしているユーザーと同一であればeditファイルが読み込まれる
+    if @item.user_id == current_user.id
+    else
+      redirect_to root_path
+    end
+  end
 
-  #def edit
-  #end
 
-  #def update
-  #  @item.update(item_params)
-  #  # バリデーションがOKであれば詳細画面へ
-  #  if @item.valid?
-  #    redirect_to item_path(item_params)
-  #  else
-  #    # NGであれば、エラー内容とデータを保持したままeditファイルを読み込み、エラーメッセージを表示させる
-  #    render 'edit'
-  #  end
-  #end
+  def update
+    @item.update(item_params)
+    # バリデーションがOKであれば詳細画面へ
+    if @item.valid?
+      redirect_to item_path(item_params)
+    else
+      # NGであれば、エラー内容とデータを保持したままeditファイルを読み込み、エラーメッセージを表示させる
+      render 'edit'
+    end
+  end
 
   #def show
   #end
