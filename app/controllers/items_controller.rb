dad676d12c6e4f29e5b6231cde5cac24dc63a1f3
suzikibsk <96762887+suzikibsk@users.noreply.github.com>
2022-05-13 11:12:12 +0900

    商品削除機能の実装　修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,10 +46,9 @@ class ItemsController < ApplicationController
   def destroy
     # ログインしているユーザーと同一であればデータを削除する
     if @item.user_id == current_user.id
-      @item.destroy
-    else
-      redirect_to root_path
+         @item.destroy
     end
+   redirect_to root_path
   end
 
   private
