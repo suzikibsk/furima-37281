aef2610507d6d8b04d9b602dc51ec22e7a4670c0
suzikibsk <96762887+suzikibsk@users.noreply.github.com>
2022-05-13 14:45:05 +0900

    クレジット決済の機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :non_purchased_item, only: [:index, :create]
   def index
     @order_form = OrderForm.new
     # item_idを指定することで、マイグレーションファイルに対応して、リファレンス(参照)され、itemsテーブルにあるデータを取得できる
@@ -28,6 +29,20 @@ class OrdersController < ApplicationController
     params.require(:order_form).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,        # 商品の値段
+      card: order_params[:token], # カードトークン
+      currency: 'jpy'             # 通貨の種類（日本円）
+    )
+  end
+
+  def non_purchased_item
+    # itemがあっての、order_form（入れ子構造）。他のコントローラーで生成されたitemを使うにはcreateアクションに定義する。
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
+  end
 end
 
 
