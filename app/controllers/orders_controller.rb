32eb82e50606a7835ca1a59f0fa81d3dcb9cfef9
suzikibsk <96762887+suzikibsk@users.noreply.github.com>
2022-05-16 16:33:37 +0900

    クレジット機能の変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,10 +11,13 @@ class OrdersController < ApplicationController
 
   def create
     @order_form = OrderForm.new(order_params)
-    # itemがあっての、order_form（入れ子構造）。他のコントローラーで生成されたitemを使うにはcreateアクションに定義が必要。
-    @item = Item.find(params[:item_id])
     if @order_form.valid?
-      pay_item
+      Payjp.api_key = "sk_test_0dd54aced4e1f31ff756864d"
+      Payjp::Charge.create(
+        amount: @item.price,           # 商品の値段
+        card: order_params[:token],    # カードトークン
+        currency: 'jpy'                 # 通貨の種類（日本円）
+      )
       @order_form.save
       redirect_to root_path
     else
@@ -29,14 +32,6 @@ class OrdersController < ApplicationController
     params.require(:order_form).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
-  def pay_item
-    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
-    Payjp::Charge.create(
-      amount: @item.price,        # 商品の値段
-      card: order_params[:token], # カードトークン
-      currency: 'jpy'             # 通貨の種類（日本円）
-    )
-  end
 
   def non_purchased_item
     # itemがあっての、order_form（入れ子構造）。他のコントローラーで生成されたitemを使うにはcreateアクションに定義する。
